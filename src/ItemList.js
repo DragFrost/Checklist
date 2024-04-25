cb67318b84ea5363adb7a4db260b7323c319844c
Anurag Paul <anuragpaul40@gmail.com>
2024-04-25 20:03:40 +0530

    Initial commit

---
+++ src/ItemList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Lineitem from "./Lineitem";
+
+const ItemList = ({items, handleCheck, handleDelete}) => {
+  return (
+    <ul>{items.map((item) => (
+        <Lineitem 
+            key={item.id}
+            item={item}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+        />
+    ))}
+</ul>
+  )
+}
+
+export default ItemList
