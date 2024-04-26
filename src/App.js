76a524217bc758f9b67193ad3fdb5b2a70c57bc2
Dwaipayan Dutta <dwaipayandutta.official@gmail.com>
2024-04-27 01:07:08 +0530

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,76 +1,74 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-import AddItem from "./components/AddItem";
-import SearchItem from "./components/SearchItem";
+import { useState } from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import AddItem from './AddItem';
+import SearchItem from './SearchItem';
 
 function App() {
-  const [items, setItems] = useState(
-    localStorage.getItem("taskList")
-      ? JSON.parse(localStorage.getItem("taskList"))
-      : []
-  );
+
+  const [items, setItems] = useState( localStorage.getItem("taskList") ? JSON.parse(localStorage.getItem("taskList")) : [] );
 
   const [dark, setDark] = useState(JSON.parse(localStorage.getItem("bgcolor")));
 
-  const [newItem, setNewItem] = useState("");
+  const [newItem, setNewItem] = useState('');
 
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
-    localStorage.setItem("taskList", JSON.stringify(newItems));
-  };
+    localStorage.setItem('taskList', JSON.stringify(newItems));
+  }   
 
-  const addItem = (item) => {
-    let counter;
-    if (!localStorage.getItem("counter")) {
+  const addItem = (item) => {  
+    let counter; 
+    if (!localStorage.getItem('counter')) {
       counter = 0;
-    }
-    counter = JSON.parse(localStorage.getItem("counter"));
+    }  
+    counter = JSON.parse(localStorage.getItem('counter'));
     counter++;
-    localStorage.setItem("counter", JSON.stringify(counter));
-    const myNewItem = { id: counter, checked: false, item };
+    localStorage.setItem('counter', JSON.stringify(counter));
+    const myNewItem = {id: counter, checked: false, item};
     const listItems = [...items, myNewItem];
     setAndSaveItems(listItems);
-  };
+  }
 
   const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setAndSaveItems(listItems);
-  };
+  }
 
   const handleDelete = (id) => {
-    const listItems = items.filter((item) => {
-      return item.id !== id;
-    });
+    const listItems = items.filter((item) => { return item.id !== id });
     setAndSaveItems(listItems);
-  };
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault();    
     if (!newItem) return;
     addItem(newItem);
-    setNewItem("");
-    console.log(JSON.parse(localStorage.getItem("counter")));
-  };
+    setNewItem('');
+    console.log(JSON.parse(localStorage.getItem('counter')));
+  }
+
 
   return (
-    <div className={dark === true ? "App dark" : "App"}>
-      <Header dark={dark} setDark={setDark} />
-      <AddItem
+    <div className={dark === true ? 'App dark' : 'App'}>
+      <Header
+        dark={dark}
+        setDark={setDark}
+      />
+      <AddItem 
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem search={search} setSearch={setSearch} />
+      <SearchItem 
+        search={search}
+        setSearch={setSearch}
+      />
       <Content
-        items={items.filter((item) =>
-          item.item.toLowerCase().includes(search.toLowerCase())
-        )}
+        items={items.filter((item) => ((item.item).toLowerCase()).includes(search.toLowerCase()) ) }
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
