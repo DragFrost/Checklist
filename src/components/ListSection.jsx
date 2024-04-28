99d78b038c5b511a8fff59d71115e1b420e0ebb2
Dwaipayan Dutta <dwaipayan.codes@gmail.com>
2024-04-28 15:53:50 +0530

    fixes

--- src/components/ListSection.jsx
+++ src/components/ListSection.jsx
@@ -4,7 +4,7 @@ import TaskComponent from "./TaskComponent";
 import NavButton from "./NavButton";
 import cross from "../assets/SVGs/cross.svg";
 
-const ListSection = ({ isOpen, setIsOpen }) => {
+const ListSection = ({ isOpen, setIsOpen, bgColor, descBGColor }) => {
   function getRandomPastelColor() {
     const hue = Math.floor(Math.random() * 360);
     const saturation = Math.floor(Math.random() * 41) + 30;
@@ -13,37 +13,22 @@ const ListSection = ({ isOpen, setIsOpen }) => {
 
     return color;
   }
-
-  // const [isVisible, setIsVisible] = useState("hidden");
   const [isAddWindowOpen, setIsAddWindowOpen] = useState("hidden");
-
-  // const visiblity = "block"
-  // function handleTaskAdd() {
-  //   if (isVisible == "visible") {
-  //     setIsVisible("hidden");
-  //   } else {
-  //     setIsVisible("visible");
-  //   }
-
-  //   console.log("anurag laura");
-  // }
+  const [addButtonDisabled, setaddButtonDisabled] = useState(false)
 
   function handleAddWindow() {
     if (isAddWindowOpen == "visible") {
       setIsAddWindowOpen("hidden");
+      setaddButtonDisabled(false)
     } else {
       setIsAddWindowOpen("visible");
+      setaddButtonDisabled(true)
     }
 
     console.log("anurag laura");
   }
 
   function handleClick() {
-    // if (isVisible == "visible") {
-    //   setisVisible("hidden");
-    // } else {
-    //   setisVisible("visible");
-    // }
     setIsOpen("hidden");
 
     console.log("anurag laura");
@@ -72,6 +57,7 @@ const ListSection = ({ isOpen, setIsOpen }) => {
             />
             <button
               onClick={handleAddWindow}
+              disabled={addButtonDisabled}
               className="text-black p-2 bg-blue-400 border-[1px] w-[20%] border-black rounded-lg text-center"
             >
               add task
@@ -81,19 +67,19 @@ const ListSection = ({ isOpen, setIsOpen }) => {
             </button>
           </div>
           <div className={isAddWindowOpen}>
-            <div className="bg-slate-400 border-black border-[1px] rounded-xl w-[95%] h-[20%] p-2 m-1 flex justify-between">
+            <div className="bg-slate-400 border-black border-[1px] rounded-xl w-[95%] h-[20%] p-2 m-1 ml-6 flex justify-between">
               <input
                 type="text"
                 className="w-[80%] rounded-lg bg-orange-200 text-black p-2 border-black border-[1px]"
               />
               <div className="w-[20%] flex flex-col justify-between items-center">
-                <button className=" text-slate-800 p-2 bg-purple-400 border-[1px] w-[60%] border-black rounded-lg flex justify-center items-center">
+                <button className=" text-slate-800 p-2 bg-purple-400 border-[1px] w-[60%] h-[45%] border-black rounded-lg flex justify-center items-center">
                   Save
                   {/* add svg */}
                 </button>
                 <button
                   onClick={handleAddWindow}
-                  className=" text-slate-800 p-2 bg-purple-400 border-[1px] w-[60%] border-black rounded-lg flex justify-center items-center"
+                  className=" text-slate-800 p-2 bg-purple-400 border-[1px] w-[60%] h-[45%] border-black rounded-lg flex justify-center items-center"
                 >
                   Cancel
                   {/* add svg */}
