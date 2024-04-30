61cb87c573b62882aa2a90f32aa7cbe1b70893ab
Dwaipayan Dutta <dwaipayan.codes@gmail.com>
2024-05-01 01:45:56 +0530

    changed input to text area

--- src/components/ListSection.jsx
+++ src/components/ListSection.jsx
@@ -11,7 +11,7 @@ const ListSection = ({
   descBGColor,
   task,
   setTask,
-  taskId
+  taskId,
 }) => {
   function getRandomPastelColor() {
     const hue = Math.floor(Math.random() * 360);
@@ -23,7 +23,7 @@ const ListSection = ({
   }
   const [isAddWindowOpen, setIsAddWindowOpen] = useState("hidden");
   const [addButtonDisabled, setaddButtonDisabled] = useState(false);
-  const [addInput, setAddInput] = useState('');
+  const [addInput, setAddInput] = useState("");
 
   function handleAddWindow() {
     if (isAddWindowOpen == "visible") {
@@ -37,23 +37,27 @@ const ListSection = ({
 
   const handleAdd = () => {
     let counter;
-    if (!localStorage.getItem('counter')) {
+    if (!localStorage.getItem("counter")) {
       counter = 0;
     }
-    counter = JSON.parse(localStorage.getItem('counter'));
+    counter = JSON.parse(localStorage.getItem("counter"));
     counter++;
-    localStorage.setItem('subCounter', JSON.stringify(counter));
+    localStorage.setItem("subCounter", JSON.stringify(counter));
     const subId = counter;
     const subTask = addInput;
     const subTaskColor = getRandomPastelColor();
-    const newTask = { subId, subTask, subTaskColor }
-    const listItems = task.map((item) => item.id === taskId ? { ...item, subTasks: [...item.subTasks, newTask] } : item);
+    const newTask = { subId, subTask, subTaskColor };
+    const listItems = task.map((item) =>
+      item.id === taskId
+        ? { ...item, subTasks: [...item.subTasks, newTask] }
+        : item
+    );
     setTask(listItems);
-    setAddInput('');
+    setAddInput("");
     setIsAddWindowOpen("hidden");
     setaddButtonDisabled(false);
     localStorage.setItem("task", JSON.stringify(listItems));
-  }
+  };
 
   function handleClick() {
     setIsOpen("hidden");
@@ -70,11 +74,14 @@ const ListSection = ({
           className="h-[96.5%] w-[70%] rounded-2xl py-4 px-6  border-black border-[1px]"
         >
           <div className="text-black text-2xl p-2 mb-1 flex justify-between items-center">
-            {task.map((item) => item.id === taskId ? item.title : '')}
+            {task.map((item) => (item.id === taskId ? item.title : ""))}
             <NavButton bgColor={"#FF0000"} svg={cross} func={handleClick} />
           </div>
-          <h1 style={{ backgroundColor: descBGColor ? descBGColor : "#99f191" }} className="text-black mb-4 p-2 rounded-lg">
-            {task.map((item) => item.id === taskId ? item.description : '')}
+          <h1
+            style={{ backgroundColor: descBGColor ? descBGColor : "#99f191" }}
+            className="text-black mb-4 p-2 rounded-lg"
+          >
+            {task.map((item) => (item.id === taskId ? item.description : ""))}
           </h1>
           <div className="flex justify-evenly">
             <input
@@ -101,14 +108,25 @@ const ListSection = ({
           </div>
           <div className={isAddWindowOpen}>
             <div className="bg-slate-400 border-black border-[1px] rounded-xl w-[95%] h-[20%] p-2 m-1 ml-6 flex justify-between">
-              <input
+              {/* <input
                 type="text"
-                className="w-[80%] rounded-lg bg-orange-200 text-black p-2 border-black border-[1px]"
+                className="w-[80%] rounded-lg bg-orange-200 text-black p-2 border-black border-[1px] text-wrap flex flex-wrap"
+                value={addInput}
+                onChange={(e) => setAddInput(e.target.value)}
+              /> */}
+              <textarea
+                rows="4"
+                cols="50"
+                className="w-[80%] rounded-lg bg-orange-200 text-black p-2 border-black border-[1px] text-wrap flex flex-wrap"
                 value={addInput}
                 onChange={(e) => setAddInput(e.target.value)}
               />
+
               <div className="w-[20%] flex flex-col justify-between items-center">
-                <button className=" text-slate-800 p-2 bg-purple-400 border-[1px] w-[60%] h-[45%] border-black rounded-lg flex justify-center items-center" onClick={handleAdd}>
+                <button
+                  className=" text-slate-800 p-2 bg-purple-400 border-[1px] w-[60%] h-[45%] border-black rounded-lg flex justify-center items-center"
+                  onClick={handleAdd}
+                >
                   Save
                   {/* add svg */}
                 </button>
@@ -125,14 +143,17 @@ const ListSection = ({
           <div className="mt-4 p-2 flex flex-col justify-center">
             <h1 className="text-black mb-1">Things to do:</h1>
             <div className="flex flex-col w-full">
-              {task.map((item) => item.id === taskId && item.subTasks.map((subItem) => (
-                <TaskComponent
-                  key={subItem.subId}
-                  taskText={subItem.subTask}
-                  bgColor={subItem.subTaskColor}
-                />
-              )))}
-
+              {task.map(
+                (item) =>
+                  item.id === taskId &&
+                  item.subTasks.map((subItem) => (
+                    <TaskComponent
+                      key={subItem.subId}
+                      taskText={subItem.subTask}
+                      bgColor={subItem.subTaskColor}
+                    />
+                  ))
+              )}
             </div>
           </div>
         </div>
