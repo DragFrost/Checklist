d3724aafdeb22f166fcae968a627d2f655d00960
Dwaipayan Dutta <dwaipayan.codes@gmail.com>
2024-05-01 01:18:24 +0530

    added optimisations

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
-const NavButton = ({bgColor, func, svg}) => {
-    const currentBG = bgColor ? bgColor : "#FFFFFF"; //"#99ff99";
+const NavButton = ({ bgColor, func, svg, alt }) => {
+  const currentBG = bgColor ? bgColor : "#FFFFFF"; //"#99ff99";
   return (
-    <button onClick={func} style={{backgroundColor: currentBG}} className="h-10 w-10 rounded-3xl text-black flex justify-center items-center mt-1 border-[1px] border-black">
-      <img src={svg} alt="Logo" />
+    <button
+      onClick={func}
+      style={{ backgroundColor: currentBG }}
+      className="h-10 w-10 rounded-3xl text-black flex justify-center items-center mt-1 border-[1px] border-black"
+    >
+      <img src={svg} alt={alt ? alt : "none"} />
     </button>
   );
 };
